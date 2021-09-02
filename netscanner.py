61dbf46e62c63486ab2e5264c56b6211ef7cf7a0
Eren Saylan <74628933+thesaep@users.noreply.github.com>
2021-09-02 20:43:56 +0300

    Update netscanner.py

--- netscanner.py
+++ netscanner.py
@@ -3,7 +3,7 @@ import optparse
 
 
 
-def get_user_input():
+def get_input():
     parse_object = optparse.OptionParser()
     parse_object.add_option("-i","--ipaddress", dest="ip_address",help="Enter IP Address")
 
@@ -14,14 +14,12 @@ def get_user_input():
 
     return user_input
 
-def scan_my_network(ip):
-    arp_request_packet = scapy.ARP(pdst=ip)                  
-    #scapy.ls(scapy.ARP())                                    
-    broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")  
-    #scapy.ls(scapy.Ether())                                  
+def scan_network(ip):
+    arp_request_packet = scapy.ARP(pdst=ip)                                                      
+    broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")                                    
     combined_packet = broadcast_packet/arp_request_packet     
     (answered_list,unanswered_list) = scapy.srp(combined_packet,timeout=1) 
     answered_list.summary()    
 
-user_ip_address = get_user_input()
-scan_my_network(user_ip_address.ip_address)
+user_ip_address = get_input()
+scan_network(user_ip_address.ip_address)
